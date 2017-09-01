871d9d885a94fc75c53c8909a168559b43e22127
congzhaoyang <congzhaoyang1@163.com>
2017-09-01 11:34:48 +0800

    add demo05, add url-loader for img, add sas-loader for .scss files

---
+++ demo05/main.js
@@ -0,0 +1,9 @@
+require('./index.scss')
+
+var imgDog1 = document.createElement('img')
+imgDog1.src = require('./dog1.jpg')
+document.body.appendChild(imgDog1)
+
+var imgDog2 = document.createElement('img')
+imgDog2.src = require('./dog2.jpg')
+document.body.appendChild(imgDog2)
---
+++ demo05/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings 
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS 
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS 
+                }]               
+            }
+        ],
+    }
+}
