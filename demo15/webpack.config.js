040adb856b6240c43a1c03912955ad816a0de676
congzhaoyang <congzhaoyang1@163.com>
2017-09-02 10:53:44 +0800

    add demo15, add hot module replacement plugin

---
+++ demo15/webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: [
+    './index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      },
+      include: path.join(__dirname, '.')
+    }]
+  }
+};
